62a772c86890aeb6d3d0e9cd9be614cc24e1c5c9
Ekemini-Udofia <ekeminiabasiudofia@gmail.com>
2026-09-19 21:10:25 +0100

    Added support for long press events

--- main/main.c
+++ main/main.c
@@ -20,6 +20,10 @@ static void button_double_click_callback(void *arg, void *usr_data) {
 	ESP_LOGI(TAG, "Button double click");
 }
 
+static void button_long_press_callback(void *arg, void *usr_data) {
+	ESP_LOGI(TAG, "Button long press");
+}
+
 void app_main(void) {
 	const button_config_t btn_cfg = {0};
 	const button_gpio_config_t btn_gpio_cfg = {
@@ -39,6 +43,9 @@ void app_main(void) {
 	
 	// Register double click event
 	iot_button_register_cb(gpio_btn, BUTTON_DOUBLE_CLICK, NULL, button_double_click_callback, NULL);
+
+	// Register long press event
+	iot_button_register_cb(gpio_btn, BUTTON_LONG_PRESS_HOLD, NULL, button_long_press_callback, NULL);
 
 	while (1) {
 		vTaskDelay(pdMS_TO_TICKS(1000));
